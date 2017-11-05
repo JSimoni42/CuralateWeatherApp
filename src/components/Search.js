a425b8aa2b81bf7d0c8d966b6297b3ff65adc1ca
John Simoni <jsimoni03@gmail.com>
2017-11-04 20:16:31 -0700

    Added location button with styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import location from "../img/location.svg"
 import '../styles/search.css';
 
 class Search extends Component {
@@ -9,20 +10,28 @@ class Search extends Component {
     }
 
     this.handleInput = this.handleInput.bind(this);
+    this.getLocation = this.getLocation.bind(this);
   }
 
   handleInput(event) {
     this.setState({ search: event.target.value });
   }
 
+  getLocation(event) {
+    console.log(event);
+  }
+
   render() {
     return(
       <div id="search-component">
         <h1>Enter Location Name</h1>
-        <form onSubmit={() => this.props.handleSearch(this.state.search)}>
-          <input type="text" value={this.state.search} onChange={this.handleInput} />
-          <input type="submit" value="Search" />
-        </form>
+        <div id="input-container">
+          <form onSubmit={() => this.props.handleSearch(this.state.search)}>
+            <input type="text" value={this.state.search} onChange={this.handleInput} />
+            <input type="submit" value="Search" />
+          </form>
+          <img id="location-button" src={location} onClick={this.getLocation} />
+        </div>
       </div>
     );
   }
