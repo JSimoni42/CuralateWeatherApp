2f2e90120c0934ac52737ece189f996d97289a51
John Simoni <jsimoni03@gmail.com>
2017-11-05 20:38:17 -0800

    Refactored example, added styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import './App.css';
-require('dotenv').config();
+import dotenv from 'dotenv';
 
 class App extends Component {
   constructor(){
@@ -12,53 +11,39 @@ class App extends Component {
       page: "search",
       api_ready: false
     }
+    dotenv.config();
     this.navigate = this.navigate.bind(this);
-    this.onChange = (address) => this.setState({ address });
   }
 
   navigate(page){
     this.setState({ page })
   }
 
-  componentDidMount(){
-    const script = document.createElement("script");
-    console.log(process.env);
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDsyK__nnyR25xifF6O_zOWAsTvsbmR3fw&libraries=places`;
-
-    script.onload = () => {
-      this.setState({ api_ready: true });
-    }
-
-    document.body.appendChild(script);
-  }
-
-  handleFormSubmit = (event) => {
-    event.preventDefault()
-
-    geocodeByAddress(this.state.address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error))
-  }
-
   render() {
-    const inputProps = {
-      value: this.state.address,
-      onChange: this.onChange,
-    }
+    // const inputProps = {
+    //   value: this.state.address,
+    //   onChange: this.onChange,
+    // }
+    //
+    // if (this.state.api_ready) {
+    //   return (
+    //     <form onSubmit={this.handleFormSubmit}>
+    //       <PlacesAutocomplete inputProps={inputProps} />
+    //       <button type="submit">Submit</button>
+    //     </form>
+    //   );
+    // } else {
+    //   return (
+    //     <h1>Nothing...</h1>
+    //   )
+    // }
 
-    if (this.state.api_ready) {
-      return (
-        <form onSubmit={this.handleFormSubmit}>
-          <PlacesAutocomplete inputProps={inputProps} />
-          <button type="submit">Submit</button>
-        </form>
-      );
-    } else {
-      return (
-        <h1>Nothing...</h1>
-      )
-    }
+    return(
+      <div className="App">
+        <Navbar onNav={this.navigate}/>
+        <Content page="search"/>
+      </div>
+    );
   }
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import location from "../img/location.svg";
 import '../styles/search.css';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       search: "",
-      address: '',
+      api_ready: false,
     }
 
     this.handleInput = this.handleInput.bind(this);
     this.getLocation = this.getLocation.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  handleInput(event) {
-    this.setState({ search: event.target.value });
+  componentWillMount() {
+    const script = document.createElement("script");
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDsyK__nnyR25xifF6O_zOWAsTvsbmR3fw&libraries=places";
+
+    script.onload = () => {
+      this.setState({ api_ready: true });
+    }
+
+    document.body.appendChild(script);
+  }
+
+  handleInput(new_search) {
+    console.log(new_search);
+    this.setState({ search: new_search });
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.search)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error("Error", error));
   }
 
   getLocation(event) {
@@ -29,11 +52,20 @@ class Search extends Component {
   }
 
   render() {
+    const inputProps = {
+      value: this.state.search,
+      onChange: this.handleInput,
+    }
+
+    const cssClasses = {
+      root: 'autocomplete-container',
+    }
 
     return(
       <div id="search-component">
         <h1>Enter Location Name</h1>
-        <form onSubmit={() => this.props.handleSearch({query_string: this.state.search})}>
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.api_ready ? <PlacesAutocomplete inputProps={inputProps} classNames={cssClasses}/> : ""}
           <input type="submit" value="Search" />
         </form>
         <img id="location-button" src={location} onClick={this.getLocation} alt="location-button" />
@@ -41,5 +73,7 @@ class Search extends Component {
     );
   }
 }
+
+// <form onSubmit={() => this.props.handleSearch({query_string: this.state.search})}>
 
 export default Search;
