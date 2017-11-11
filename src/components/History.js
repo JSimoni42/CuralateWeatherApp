dc234be664c44bcdda936ff2d6a33ab82ccf58e5
John Simoni <jsimoni03@gmail.com>
2017-11-11 13:59:35 -0800

    Get fake data for weather history

--- src/components/History.js
+++ src/components/History.js
@@ -9,13 +9,16 @@ import {LineChart,
 import '../styles/current.css';
 import { data, descriptions } from '../FakeGraphData';
 import DarkSkyApi from 'dark-sky-api';
+import moment from 'moment';
 
 class History extends Component {
   constructor(){
     super();
     this.state = {
       currentDescription: 0,
+      history: ""
     }
+    DarkSkyApi.apiKey = '70cb406e46fe0848ebbd51bc18b2a2df';
     this.changeDescription = this.changeDescription.bind(this);
   }
 
@@ -27,27 +30,22 @@ class History extends Component {
   }
 
   render() {
-    // return(
-    //   <div>
-    //     <h1>Past 7 Days</h1>
-    //     <h2>{this.props.location.place_name}</h2>
-    //     <h3>{this.props.location.coords.latitude}, {this.props.location.coords.longitude}</h3>
-    //     <LineChart className="current-chart" width={800} height={400} data={data} onClick={this.changeDescription}>
-    //       <Line type="monotone" dataKey="temp" stroke="#8884d8" />
-    //       <Line type="monotone" dataKey="humidity" stroke="#1a1a1a" />
-    //       <CartesianGrid stroke="#ccc" />
-    //       <XAxis dataKey="name" />
-    //       <YAxis />
-    //       <Tooltip />
-    //     </LineChart>
-    //     <p>{descriptions[this.state.currentDescription].desc}</p>
-    //     <img className="weather-icon" src={descriptions[this.state.currentDescription].icon} alt="weather-icon" />
-    //     <p>Click on another day to see its description above</p>
-    //   </div>
-    // );
-    return (
+    return(
       <div>
-       Wenis
+        <h1>Past 7 Days</h1>
+        <h2>{this.props.location.place_name}</h2>
+        <h3>{this.props.location.coords.latitude}, {this.props.location.coords.longitude}</h3>
+        <LineChart className="current-chart" width={800} height={400} data={data} onClick={this.changeDescription}>
+          <Line type="monotone" dataKey="temp" stroke="#8884d8" />
+          <Line type="monotone" dataKey="humidity" stroke="#1a1a1a" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+        <p>{descriptions[this.state.currentDescription].desc}</p>
+        <img className="weather-icon" src={descriptions[this.state.currentDescription].icon} alt="weather-icon" />
+        <p>Click on another day to see its description above</p>
       </div>
     );
   }
